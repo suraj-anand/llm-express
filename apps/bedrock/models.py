a0d3351f4870fb09cc310bcab241320f131e0e7a
Suraj <suraj02anand@gmail.com>
2024-07-30 23:20:17 +0530

    create UserBedrockModels to track user created bedrock models

--- apps/bedrock/models.py
+++ apps/bedrock/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from apps.api.models import User
 
 # Create your models here.
+class UserBedrockModels(models.Model):
+    id = models.TextField(primary_key=True, default=uuid.uuid4)
+    model_id = models.TextField(null=True, blank=True, default="")
+    model_name = models.TextField(null=True, blank=True, default="")
+    input_modalities = models.TextField(null=True, blank=True, default="")
+    output_modalities = models.TextField(null=True, blank=True, default="")
+    user_created = models.ForeignKey(User, on_delete=models.CASCADE)
+    local_path = models.TextField(null=True, blank=True, default="")
+    is_valid = models.BooleanField(null=True, blank=True, default=True)
