66d2aeaafdc244f343ce39c24102bc9103983f81
Suraj <suraj02anand@gmail.com>
2024-08-01 00:33:05 +0530

    added created_time field

--- apps/bedrock/models.py
+++ apps/bedrock/models.py
@@ -6,6 +6,7 @@ from apps.api.models import User
 class UserBedrockModels(models.Model):
     id = models.TextField(primary_key=True, default=uuid.uuid4)
     model_id = models.TextField(null=True, blank=True, default="")
+    created_time = models.DateTimeField(auto_now_add=True)
     model_name = models.TextField(null=True, blank=True, default="")
     input_modalities = models.TextField(null=True, blank=True, default="")
     output_modalities = models.TextField(null=True, blank=True, default="")
